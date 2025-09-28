8081dade39b8552342bf1b1d42e60c3b8aac9d45
loihnt05 <23520869@gm.uit.edu.vn>
2025-09-28 14:10:08 +0700

    refix implicit string

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
-API_AUDIENCE = os.getenv("API_AUDIENCE")
+API_AUDIENCE = os.getenv("API_AUDIENCE") 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from typing import Optional
+from app.routers import pet
 from fastapi import FastAPI, Depends  
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import create_tables
 from contextlib import asynccontextmanager
+from app.core.config import AUTH0_DOMAIN
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,14 +25,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-oauth2_scheme = OAuth2AuthorizationCodeBearer(  
-    tokenUrl="https://{DOMAIN}/oauth/token",  
-    authorizationUrl="https://{DOMAIN}/authorize",  
-    refreshUrl="https://{DOMAIN}/oauth/token",
-    scopes={"openid": "description", "profile": "description", "email": "description"}  
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl=f"https://{AUTH0_DOMAIN}/oauth/token",
+    authorizationUrl=f"https://{AUTH0_DOMAIN}/authorize",
+    refreshUrl=f"https://{AUTH0_DOMAIN}/oauth/token",
+    scopes={"openid": "description", "profile": "description", "email": "description"}
 )
  
- 
+app.include_router(pet.router)
+
 @app.get("/auth",)  
 def auth_required(token: Optional[str] = Depends(oauth2_scheme)):  
   return {"Logged in"}
